08045e070f413a68f7bc62d4ed509f53e65f89f2
Lucas01PL <lucas01_ce@hotmail.com>
2017-10-08 12:17:23 -0300

    Criação dos casos de teste das classes service e PasswordUtils.

--- src/main/java/br/com/mysystems/pontointeligente/api/services/EmpresaService.java
+++ src/main/java/br/com/mysystems/pontointeligente/api/services/EmpresaService.java
@@ -1,5 +1,24 @@
 package br.com.mysystems.pontointeligente.api.services;
 
+import java.util.Optional;
+
+import br.com.mysystems.pontointeligente.api.entities.Empresa;
+
 public interface EmpresaService {
 
+	/**
+	 * Retorna uma empresa dado um CNPJ.
+	 * 
+	 * @param cnpj
+	 * @return Optional<Empresa>
+	 */
+	Optional<Empresa> buscarPorCnpj(String cnpj);
+	
+	/**
+	 * Cadastra uma nova empresa na base de dados.
+	 * 
+	 * @param empresa
+	 * @return Empresa
+	 */
+	Empresa persistir(Empresa empresa);
 }
